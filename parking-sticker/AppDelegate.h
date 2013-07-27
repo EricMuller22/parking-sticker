ce1cb2c36956da89b22684b066d8dd9a9acce96b
Eric Muller <EricMuller22@gmail.com>
2013-07-26 17:11:01 -0700

    Blank project

---
+++ parking-sticker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  parking-sticker
+//
+//  Created by Eric Muller on 7/26/13.
+//  Copyright (c) 2013 Unexplored Novelty, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ parking-sticker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  parking-sticker
+//
+//  Created by Eric Muller on 7/26/13.
+//  Copyright (c) 2013 Unexplored Novelty, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
