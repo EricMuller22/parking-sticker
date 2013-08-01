622b57b73868604e17dbec8e4630b2ad42d3af56
Eric Muller <EricMuller22@gmail.com>
2013-07-31 22:59:04 -0700

    Local notification proof of concept

--- parking-sticker/TimingViewController.m
+++ parking-sticker/TimingViewController.m
@@ -8,6 +8,7 @@
 
 #import "TimingViewController.h"
 #import "NSDate+Escort.h"
+#import "UIColor+Hex.h"
 
 #define kCleaningWeeks @[@"1st and 3rd", @"2nd and 4th"]
 #define kCleaningDays @[@"Monday", @"Tuesday", @"Wednesday", @"Thursday", @"Friday", @"Saturday", @"Sunday"]
@@ -45,6 +46,18 @@
     self.picker.dataSource = self;
     self.picker.delegate = self;
     [self.view addSubview:self.picker];
+    
+    // Street cleaning activator
+    self.activator = [[UISwitch alloc] init];
+    CGRect switchFrame = self.activator.frame; // to do - autolayout
+    switchFrame.origin.y = self.picker.frame.size.height;
+    switchFrame.origin.x = self.view.frame.size.width / 2 - switchFrame.size.width / 2;
+    self.activator.frame = switchFrame;
+    [self.activator addTarget:self
+                       action:@selector(switchChanged:)
+             forControlEvents:UIControlEventValueChanged];
+    self.activator.onTintColor = [UIColor colorFromHexString:@"#D35400"]; // to do - debug tint color inheritance
+    [self.view addSubview:self.activator];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -77,6 +90,20 @@
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
 {
     [[NSUserDefaults standardUserDefaults] setInteger:row forKey: (component == 0) ? kCleaningWeeksKey : kCleaningDaysKey];
+}
+
+# pragma mark - UISwitch handler
+
+- (void)switchChanged:(UISwitch *)target
+{
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+    if (target == self.activator && target.on) {
+        UILocalNotification *streetCleaningNotification = [[UILocalNotification alloc] init];
+        streetCleaningNotification.alertBody = @"You might want to move your car soon (Street Cleaning)";
+        streetCleaningNotification.timeZone = [NSTimeZone localTimeZone];
+        streetCleaningNotification.fireDate = [NSDate dateWithMinutesFromNow:1]; // for testing
+        [[UIApplication sharedApplication] scheduleLocalNotification:streetCleaningNotification];
+    }
 }
 
 @end
